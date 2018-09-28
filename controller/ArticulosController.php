5dfb337bfff66f38d5650cbf3bfe675d8b541cd9
SIRomaniejko <siromaniejko@gmail.com>
2018-09-28 11:20:54 -0300

    modifique unas cosas para que ande mejor

--- controller/ArticulosController.php
+++ controller/ArticulosController.php
@@ -1,16 +1,23 @@
 <?php
     require('libs/Smarty.class.php');
+    require('model/ArticulosModel.php');
+    require('view/ArticulosView.php');
     class ArticulosController{
+        $model;
+        $view;
+        function __construct() {
+
+        }
         function test(){
             $a = [
-                "portada" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiS4dJvlASutabk2CJuL2OhSdZLsXudz0PRbZaJH4N8srwr6e3qQ",
+                "portada" => "https://zone1-vgu.netdna-ssl.com/wp-content/uploads/2017/09/Danganronpa-V3-04.jpg",
                 "titulo" => "juegos cristianos",
                 "descripccion" => "juegos creador por aaaa",
             ];
             $b = [
                 "portada" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiS4dJvlASutabk2CJuL2OhSdZLsXudz0PRbZaJH4N8srwr6e3qQ",
                 "titulo" => "juegos cristianos",
-                "descripccion" => "juegos creador por aaaa",
+                "descripccion" => "mmmmmmmmmmmmmm mmmmmmmmmmmmmm mmmmmmmmmmmmmmm mmmmmmmmmmmm mmmmmmmmmmmm mmmmmmmmmmm mmmmmmmmmmmmm",
             ];
             $c = [
                 "id_categoria" => 3,
