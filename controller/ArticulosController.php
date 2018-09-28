d479656ba78740a43b527536c550064f718a803e
SIRomaniejko <siromaniejko@gmail.com>
2018-09-28 11:31:45 -0300

    editado controller y view

--- controller/ArticulosController.php
+++ controller/ArticulosController.php
@@ -3,32 +3,15 @@
     require('model/ArticulosModel.php');
     require('view/ArticulosView.php');
     class ArticulosController{
-        $model;
-        $view;
+        $ArticulosModel;
+        $ArticulosView;
         function __construct() {
-            
+            $ArticulosModel = new ArticulosModel();
+            $ArticulosView = new ArticulosView();
         }
-        function test(){
-            $a = [
-                "portada" => "https://zone1-vgu.netdna-ssl.com/wp-content/uploads/2017/09/Danganronpa-V3-04.jpg",
-                "titulo" => "juegos cristianos",
-                "descripccion" => "juegos creador por aaaa",
-            ];
-            $b = [
-                "portada" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiS4dJvlASutabk2CJuL2OhSdZLsXudz0PRbZaJH4N8srwr6e3qQ",
-                "titulo" => "juegos cristianos",
-                "descripccion" => "mmmmmmmmmmmmmm mmmmmmmmmmmmmm mmmmmmmmmmmmmmm mmmmmmmmmmmm mmmmmmmmmmmm mmmmmmmmmmm mmmmmmmmmmmmm",
-            ];
-            $c = [
-                "id_categoria" => 3,
-                "nombre" => "juegos ateos",
-            ];
-            $categorias = array($a, $b);
-            $smarty = new Smarty();
-            
-            $smarty->assign('reviews', $categorias);
-            //$smarty->debugging = true;
-            $smarty->display('templates/home.tpl');
+        function home(){
+            $reviews = this->ArticulosModel->getReviewsHome();
+            this->ArticulosView->home($reviews);
         }
     }
 ?>
--- view/ArticulosView.php
+++ view/ArticulosView.php
@@ -1,5 +1,10 @@
 <?
     class ArticulosView.php{
-        
+        function home(){
+            $smarty = new Smarty();
+            $smarty->assign('reviews', $reviews);
+            //$smarty->debugging = true;
+            $smarty->display('templates/home.tpl');
+        }
     }
 ?>
