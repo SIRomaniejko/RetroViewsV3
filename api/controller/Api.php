cb3de967f22647a90e673a0942659ffcb0f5e8ca
kappaggaeru <oliveralautaro99@gmail.com>
2018-11-09 12:09:58 -0300

    Api encaminada

---
+++ api/controller/Api.php
@@ -0,0 +1,25 @@
+<?php
+
+class Api{
+
+
+  function __construct(){
+
+  }
+
+  public function json_response($data, $status) {
+      header("Content-Type: application/json");
+      header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+      return json_encode($data);
+  }
+
+  private function _requestStatus($code){
+     $status = array(
+       200 => "OK",
+       404 => "Not found",
+       500 => "Internal Server Error"
+     );
+     return ($status[$code])? $status[$code] : $status[500];
+   }
+}
+ ?>
